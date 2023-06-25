56b137cd340cd9d0aafd4677e6bc577e0825c351
Yana-Levchyshyna <levchyshynayana@gmail.com>
2023-06-25 04:22:29 +0300

    commit

---
+++ src/fetch_img.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+import Notiflix from 'notiflix';
+
+const BASE_URL = 'https://pixabay.com/api/';
+
+export async function fetchImg(searchTerm, currentPage) {
+  try {
+    const response = await axios.get(BASE_URL, {
+      params: {
+        key: '37711184-b39a9acb33bf742f63bced6b4',
+        q: searchTerm,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        per_page: 40,
+        page: currentPage,
+      },
+    });
+    console.log('response -->', response);
+    return response;
+  } catch (error) {
+    console.log(error);
+    Notiflix.Notify.failure('Oops! Something went wrong!');
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,18 +1,82 @@
-import Notiflix from 'notiflix';
+import { Notify } from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
-import axios from 'axios';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { fetchImg } from './fetch_img';
+import { renderGallery } from './render_img';
+fetchImg();
 
-const body = document.body;
-console.log(body);
+let currentPage;
 
-body.innerHTML = `<form class="search-form" id="search-form">
-  <input
-    type="text"
-    name="searchQuery"
-    autocomplete="off"
-    placeholder="Search images..."
-  />
-  <button type="submit">Search</button>
-</form>`;
+let searchTerm = '';
 
-// body.innerHTML += htmlMarkup;
+const refs = {
+  searchForm: document.querySelector('#search-form'),
+  gallery: document.querySelector('.gallery'),
+  searchBtn: document.querySelector('.searh-btn'),
+  loaderBtn: document.querySelector('.load-more'),
+};
+
+let galleryLightbox = new SimpleLightbox('.gallery a', {
+  captions: false,
+});
+
+refs.searchForm.addEventListener('submit', onSubmit);
+refs.loaderBtn.addEventListener('click', loadMoreImages);
+
+function onSubmit(e) {
+  e.preventDefault();
+  searchTerm = e.target.searchQuery.value.trim();
+
+  if (!searchTerm) {
+    clearSearch();
+    return Notify.failure(`Enter a search query, please.`);
+  }
+  fetchImg(searchTerm, currentPage).then(data => {
+    if (!data.data.hits.length) {
+      clearSearch();
+      return Notify.failure(
+        `Sorry, there are no images matching your search query. Please try again.`
+      );
+    }
+    Notify.success(`Hooray! We found ${data.data.totalHits} images.`);
+    refs.gallery.innerHTML = renderGallery(data.data.hits);
+    galleryLightbox.refresh();
+    if (currentPage > data.data.totalHits / 40) {
+      refs.gallery.insertAdjacentHTML(
+        'beforeend',
+        `<p class='result-text'>We're sorry, but you've reached the end of search results.</p>`
+      );
+    } else {
+      refs.loaderBtn.style.display = 'block';
+    }
+  });
+}
+
+function loadMoreImages() {
+  currentPage += 1;
+  fetchImg(searchTerm, currentPage).then(data => {
+    refs.gallery.insertAdjacentHTML('beforeend', renderGallery(data.data.hits));
+    galleryLightbox.refresh();
+
+    const { height: cardHeight } = document
+      .querySelector('.gallery')
+      .firstElementChild.getBoundingClientRect();
+    window.scrollBy({
+      top: cardHeight * 2,
+      behavior: 'smooth',
+    });
+    if (currentPage > data.data.totalHits / 40) {
+      refs.loaderBtnstyle.display = 'none';
+      refs.gallery.insertAdjacentHTML(
+        'beforeend',
+        `<p class='result-text'>We're sorry, but you've reached the end of search results.</p>`
+      );
+    }
+  });
+}
+
+function clearSearch() {
+  refs.searchForm.reset();
+  refs.gallery.innerHTML = '';
+  refs.loaderBtn.style.display = 'none';
+}
